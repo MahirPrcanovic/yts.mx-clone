18910db41d09b67d2f7032c7f9804a845f2bde4c
Mahir Prcanovic <mahirprcanovic7@gmail.com>
2022-03-02 18:42:22 +0100

    pagination fully done

--- src/components/BrowseMoivesPage/Main.js
+++ src/components/BrowseMoivesPage/Main.js
@@ -7,6 +7,7 @@ const Main = () => {
   const [pageNumber, setPageNumber] = useState(1);
   const [activePage, setActivePage] = useState(1);
   const [activeIndex, setActiveIndex] = useState(1);
+  const [lastNumber, setLastNumber] = useState(false);
   const history = useHistory();
   const funkcija = useCallback(() => {
     history.push("/browse-movies?page=1");
@@ -14,22 +15,27 @@ const Main = () => {
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const name = queryParams.get("page");
-    if (+name < 1977 && +name > 0) {
+    if (+name < 1978 && +name > 0) {
       setPageNumber(name ? +name : 1);
-      setActivePage(name ? +name - 1 : 1);
-    } else if (+name === 1977) {
-      setPageNumber(1970);
-      setActivePage(1977);
-      setActiveIndex(7);
-    } else {
+      setActivePage(name ? +name : 1);
+      setActiveIndex(0);
+    }
+    if (+name > 1977 || +name < 1) {
+      setPageNumber(1);
+      setActivePage(1);
       setActiveIndex(0);
       funkcija();
     }
-    if (+name > 1970 && +name < 1977) {
+    if (+name === 1977) {
       setPageNumber(1970);
-      setActivePage(name ? +name : 1);
+      setActivePage(1977);
+      setActiveIndex(7);
+    }
+    if (+name > 1970 && +name < 1977) {
       const index = +name - 1970;
       setActiveIndex(index);
+      setPageNumber(1970);
+      setActivePage(+name);
     }
     // eslint-disable-next-line
   }, []);
@@ -79,24 +85,23 @@ const Main = () => {
               First
             </Link>
             <Link
-              to={`/browse-movies?page=${pageNumber - 1}`}
+              to={`/browse-movies?page=${
+                activePage - 1 <= 0 ? 1 : activePage - 1
+              }`}
               className={`${classes.link} ${classes.previous}`}
               onClick={() => {
-                setPageNumber((prevNumber) => {
-                  setActivePage(pageNumber - 1);
-                  setActiveIndex((prevIndex) => {
-                    if (prevIndex > 0) {
-                      return prevIndex - 1;
-                    } else {
-                      return prevIndex;
-                    }
+                if (activeIndex === 0 && pageNumber > 1) {
+                  setPageNumber((page) => page - 8);
+                  setActivePage((pageNumber) => pageNumber - 1);
+                  setActiveIndex(7);
+                } else {
+                  // setPageNumber((page) => page - 1);
+                  setActiveIndex((index) => {
+                    if (index === 0) return 0;
+                    else return index - 1;
                   });
-                  if (prevNumber >= 2) {
-                    return prevNumber - 1;
-                  } else {
-                    return prevNumber;
-                  }
-                });
+                  setActivePage((page) => page - 1);
+                }
               }}
             >
               Previous
@@ -119,16 +124,24 @@ const Main = () => {
               );
             })}
             <Link
-              to={`/browse-movies?page=${pageNumber + 1}`}
-              className={classes.link}
+              to={`/browse-movies?page=${
+                activePage !== 1977 ? activePage + 1 : activePage
+              }`}
+              className={`${classes.link} ${
+                lastNumber ? classes.disable : " "
+              }`}
               onClick={() => {
-                setPageNumber((prevNumber) => {
-                  if (prevNumber === 1970) {
-                    return;
-                  } else {
-                    return prevNumber + 1;
-                  }
-                });
+                if (activeIndex === 7 && activePage !== 1977) {
+                  setActiveIndex(0);
+                  setActivePage((page) => page + 1);
+                  setPageNumber((page) => page + 8);
+                } else if (activeIndex < 7 && activePage !== 1977) {
+                  setActiveIndex((index) => index + 1);
+                  setActivePage((page) => page + 1);
+                }
+                if (activeIndex === 7 && activePage === 1977) {
+                  return;
+                }
               }}
             >
               Next
@@ -166,7 +179,96 @@ const Main = () => {
             })}
         </div>
       </div>
-      <div></div>
+      <div className={classes.pagination}>
+        <div className={classes.srce}>
+          <div className={classes.pages}>
+            <Link
+              to="/browse-movies?page=1"
+              className={classes.link}
+              onClick={() => {
+                setPageNumber(1);
+                setActivePage(1);
+                setActiveIndex(0);
+              }}
+            >
+              First
+            </Link>
+            <Link
+              to={`/browse-movies?page=${
+                activePage - 1 <= 0 ? 1 : activePage - 1
+              }`}
+              className={`${classes.link} ${classes.previous}`}
+              onClick={() => {
+                if (activeIndex === 0 && pageNumber > 1) {
+                  setPageNumber((page) => page - 8);
+                  setActivePage((pageNumber) => pageNumber - 1);
+                  setActiveIndex(7);
+                } else {
+                  // setPageNumber((page) => page - 1);
+                  setActiveIndex((index) => {
+                    if (index === 0) return 0;
+                    else return index - 1;
+                  });
+                  setActivePage((page) => page - 1);
+                }
+              }}
+            >
+              Previous
+            </Link>
+            {numbers.map((number, index) => {
+              return (
+                <Link
+                  to={`/browse-movies?page=${pageNumber + number}`}
+                  className={`${classes.link} ${
+                    activeIndex === number ? classes.active : " "
+                  }`}
+                  key={index}
+                  onClick={() => {
+                    setActivePage(pageNumber + number);
+                    setActiveIndex(number);
+                  }}
+                >
+                  {pageNumber + number}
+                </Link>
+              );
+            })}
+            <Link
+              to={`/browse-movies?page=${
+                activePage !== 1977 ? activePage + 1 : activePage
+              }`}
+              className={`${classes.link} ${
+                lastNumber ? classes.disable : " "
+              }`}
+              onClick={() => {
+                if (activeIndex === 7 && activePage !== 1977) {
+                  setActiveIndex(0);
+                  setActivePage((page) => page + 1);
+                  setPageNumber((page) => page + 8);
+                } else if (activeIndex < 7 && activePage !== 1977) {
+                  setActiveIndex((index) => index + 1);
+                  setActivePage((page) => page + 1);
+                }
+                if (activeIndex === 7 && activePage === 1977) {
+                  return;
+                }
+              }}
+            >
+              Next
+            </Link>
+            <Link
+              to="/browse-movies?page=1977"
+              className={classes.link}
+              onClick={() => {
+                setPageNumber(1970);
+                setActivePage(1977);
+                setActiveIndex(7);
+              }}
+            >
+              Last
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
