364a5d377cceb834cbbee370a586e30a86dd49b2
Mahir Prcanovic <mahirprcanovic7@gmail.com>
2022-03-05 14:56:04 +0100

    fixed home page bug on desktop view

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -2,9 +2,7 @@
 import classes from "./Header.module.css";
 import { Link } from "react-router-dom";
 import React from "react";
-import { useHistory } from "react-router-dom";
 const Header = (props) => {
-  const history = useHistory();
   return (
     <div
       className={classes.header}
